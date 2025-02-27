8fb4aa51b21f62fe9aa0117e84e454392c0d36d3
John Allen <jallen@braid.ai>
2025-02-27 13:41:41 -0500

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import BapForm from './BapForm'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+function Fill() {
+  return <BapForm />;
+}
+
+function Render() {
+  return <h1>Render Page</h1>;
+}
 
 function App() {
   return (
-    <div>
-      <BapForm></BapForm>
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Fill />} />
+        <Route path="/render" element={<Render />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
